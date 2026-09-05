fbd4e5a11a36041a00fd61f8453506c76333c706
GalacticJack1 <69172390+GalacticJack1@users.noreply.github.com>
2026-09-04 23:31:16 -0500

    Add files via upload

---
+++ spells.js
@@ -0,0 +1,14 @@
+const BASE_URL = "https://www.dnd5eapi.co";
+
+export async function getAllSpells() {
+  const spellIndexes = await fetch(BASE_URL + "/api/2014/spells").then((response) =>
+    response.json()
+  );
+  return Promise.all(
+    spellIndexes.results.map((index) =>
+      fetch(BASE_URL + index.url).then((response) => response.json())
+    )
+  );
+}
+
+console.log getAllSpells
